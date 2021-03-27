65083f6ddd6b82ebafded035702f7415bebf66af
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-03-27 12:46:40 +0600

    using built in usermodel

--- UserAuthenticationApp/UserAuthenticationApp/settings.py
+++ UserAuthenticationApp/UserAuthenticationApp/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR=os.path.join(BASE_DIR,'Templates')
+STATIC_DIR=os.path.join(BASE_DIR,'Static')
+MEDIA_DIR=os.path.join(BASE_DIR,'Media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -118,4 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Static/'
+STATICFILES_DIR=[STATIC_DIR,]
+
+
+MEDIA_ROOT=MEDIA_DIR
+MEDIA_URL = '/Media/'
